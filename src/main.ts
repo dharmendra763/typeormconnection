d5a705a6220c4089b332a81a375df9c1b4bce0a8
tanmay986 <136155800+tanmay986@users.noreply.github.com>
2024-02-04 22:50:24 +0530

    solved error with imporing cookie session

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import CookieSession from "cookie-session";
+const CookieSession = require("cookie-session");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
